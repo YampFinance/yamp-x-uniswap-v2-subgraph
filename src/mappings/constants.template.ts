9676cb44bc35e3f202f6b6966840775fcdf52a12
yampfinance <admin@yamp.finance>
2021-07-19 09:17:47 +0700

    update

---
+++ src/mappings/constants.template.ts
@@ -0,0 +1,4 @@
+export const YAMP_FACTORY_ADDRESS = '{{ yampFactory }}'
+export const UNISWAP_FACTORY_ADDRESS = '{{ uniswapFactory }}'
+export const WETH_ADDRESS = '{{ weth }}'
+export const STABLE_WETH_PAIR = '{{ stablePair }}'
